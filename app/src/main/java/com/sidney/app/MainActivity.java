d47ca0687d0ac6b20c6259099978e8882a1d8028
huxuelei <xuelei199015@163.com>
2019-01-29 15:06:44 +0800

    自定义BadgeView在radiobutton上增加消息提醒

--- app/src/main/java/com/sidney/app/MainActivity.java
+++ app/src/main/java/com/sidney/app/MainActivity.java
@@ -1,13 +1,17 @@
 package com.sidney.app;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.view.View;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.sidney.app.ui.one.fragment.OneFragment;
 import com.sidney.app.ui.three.fragment.ThreeFragment;
 import com.sidney.app.ui.two.fragment.TwoFragment;
+import com.sidney.app.widget.BadgeView;
 import com.sidney.devlib.ui.BaseActivity;
 import com.sidney.devlib.utils.ToastUtils;
 
@@ -17,6 +21,8 @@ public class MainActivity extends BaseActivity implements RadioGroup.OnCheckedCh
     private RadioGroup mTabMenu;
     // 正在显示的fragment
     private Fragment lastFragment;
+
+    private TextView mTvMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +36,41 @@ public class MainActivity extends BaseActivity implements RadioGroup.OnCheckedCh
         // 初始化底部菜单
         mTabMenu = (RadioGroup) findViewById(R.id.tab_menu);
         mTabMenu.setOnCheckedChangeListener(this);
+        mTvMsg = (TextView) findViewById(R.id.tv_msg);
+        remind(mTvMsg);
         //setCurrentItem(1);
         //setCurrentItem(2);
         setCurrentItem(0);
+    }
+
+    /**
+     * desc:消息提醒
+     */
+    private void remind(View view) { //BadgeView的具体使用
+        // 创建一个BadgeView对象，view为你需要显示提醒的控件
+        BadgeView badge1 = new BadgeView(this, view);
+        // 需要显示的提醒类容
+        badge1.setText("12");
+        // 显示的位置.右上角,BadgeView.POSITION_BOTTOM_LEFT,下左，还有其他几个属性
+        badge1.setBadgePosition(BadgeView.POSITION_TOP_RIGHT);
+        // 文本颜色
+        badge1.setTextColor(Color.WHITE);
+        // 提醒信息的背景颜色，自己设置
+        badge1.setBadgeBackgroundColor(Color.RED);
+        //还可以设置背景图片
+        //badge1.setBackgroundResource(R.mipmap.icon_message_png);
+        // 文本大小
+        badge1.setTextSize(12);
+        // 水平和竖直方向的间距
+        //badge1.setBadgeMargin(3, 3);
+        //各边间隔
+        badge1.setBadgeMargin(5);
+        //显示效果，如果已经显示，则隐藏，如果隐藏，则显示
+        // badge1.toggle();
+        // 显示
+        badge1.show();
+        //隐藏
+        // badge1.hide();
     }
 
 
