813142f3f5c342a2ba14990ff5a43075c81abf27
huxuelei <xuelei199015@163.com>
2020-04-10 15:02:47 +0800

    增加handler处理

--- app/src/main/java/com/sidney/app/ui/two/activity/ListviewDemoActivity.java
+++ app/src/main/java/com/sidney/app/ui/two/activity/ListviewDemoActivity.java
@@ -2,15 +2,19 @@ package com.sidney.app.ui.two.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.sidney.app.R;
 import com.sidney.devlib.comment.baseAdapter.CommonAdapter;
 import com.sidney.devlib.comment.baseAdapter.ViewHolder;
 import com.sidney.devlib.ui.BaseActivity;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,7 +25,7 @@ public class ListviewDemoActivity extends BaseActivity {
             "RecyclerView",
             "MultiItem RecyclerView"));
     private ListView mListView;
-    private View mEmptyView ;
+    private View mEmptyView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +33,14 @@ public class ListviewDemoActivity extends BaseActivity {
         setContentView(R.layout.activity_listview_demo);
         mListView = ((ListView) findViewById(R.id.id_listview_list));
         mEmptyView = findViewById(R.id.id_empty_view);
-        mListView.setAdapter(new CommonAdapter<String>(this, R.layout.item_list, mDatas)
-        {
+        mListView.setAdapter(new CommonAdapter<String>(this, R.layout.item_list, mDatas) {
             @Override
-            public void convert(ViewHolder holder, String o, int pos)
-            {
+            public void convert(ViewHolder holder, String o, int pos) {
                 holder.setText(R.id.id_item_list_title, o);
             }
 
             @Override
-            public void onViewHolderCreated(ViewHolder holder, View itemView)
-            {
+            public void onViewHolderCreated(ViewHolder holder, View itemView) {
                 super.onViewHolderCreated(holder, itemView);
             }
         });
@@ -51,14 +52,11 @@ public class ListviewDemoActivity extends BaseActivity {
 //        t2.setText("Header 2");
 //        mListView.addHeaderView(t1);
 //        mListView.addHeaderView(t2);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-            {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = null;
-                switch (position)
-                {
+                switch (position) {
                     case 0:
                     default:
                         //intent = new Intent(mActivity, MultiItemListViewActivity.class);
@@ -75,5 +73,51 @@ public class ListviewDemoActivity extends BaseActivity {
                     startActivity(intent);
             }
         });
+
+        Message message = new Message();
+        message.what = 1;
+        message.obj = "11";
+        handler.sendMessage(message);
+    }
+
+    private HandlerHolder handler = new HandlerHolder(new OnReceiveMessageListener() {
+
+        @Override
+
+        public void handlerMessage(Message msg) {
+            switch (msg.what) {
+                case 1:
+                    String ss= (String) msg.obj;
+                    break;
+            }
+        }
+    });
+
+    public static class HandlerHolder extends Handler {
+
+        private WeakReference<OnReceiveMessageListener> mListenerWeakReference;
+
+        /**
+         * @param listener 收到消息回调接口
+         */
+        HandlerHolder(OnReceiveMessageListener listener) {
+            mListenerWeakReference = new WeakReference<>(listener);
+        }
+
+
+        @Override
+
+        public void handleMessage(Message msg) {
+            if (mListenerWeakReference != null && mListenerWeakReference.get() != null) {
+                mListenerWeakReference.get().handlerMessage(msg);
+            }
+        }
+    }
+
+    /**
+     * 收到消息回调接口
+     */
+    public interface OnReceiveMessageListener {
+        void handlerMessage(Message msg);
     }
 }
--- devlib/src/main/java/com/sidney/devlib/ui/BaseActivity.java
+++ devlib/src/main/java/com/sidney/devlib/ui/BaseActivity.java
@@ -2,9 +2,8 @@ package com.sidney.devlib.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 
-public class BaseActivity extends FragmentActivity {
+public class BaseActivity extends Activity {
     public Activity mActivity;
 
     @Override
